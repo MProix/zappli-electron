0bb7202e3509e6e95740abd9aea1531dbd28edae
MProix <melanie@proix.eu>
2024-05-24 18:03:42 +0200

    with right button menu for windows users

--- views/notification/notification.js
+++
@@ -1,6 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-ipcRenderer.on('store-data', (evt, data) => {
-    console.log(data);
-    $("#message").html("<span>"+data.message+"</span><a href='"+data.url+"'>Télécharger</a><img src='"+data.img+"'>");
-})
